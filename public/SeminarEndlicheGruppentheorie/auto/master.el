fddff0d7a65de367d63923729162280af1169959
Jakob Schneider <jakob@karcs.wh2.tu-dresden.de>
2014-10-10 13:18:48 +0200

    Adding various docs.

---
+++ public/SeminarEndlicheGruppentheorie/auto/master.el
@@ -0,0 +1,9 @@
+(TeX-add-style-hook "master"
+ (lambda ()
+    (TeX-run-style-hooks
+     "latex2e"
+     "art10"
+     "article"
+     "a4paper"
+     "../Config/german")))
+
---
+++ public/StundenplanWS2014/auto/master.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook "master"
+ (lambda ()
+    (TeX-run-style-hooks
+     "multirow"
+     "amsfonts"
+     "amstext"
+     "mathrsfs"
+     "amssymb"
+     "amsmath"
+     ""
+     "kpfonts"
+     "uprightgreeks"
+     "latex2e"
+     "art10"
+     "article"
+     "a4paper"
+     "../Config/german")))
+

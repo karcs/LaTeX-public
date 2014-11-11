904d16883160461bb36852603db193bf53794fc3
Jakob Schneider <jakob.schneider@tu-dresden.de>
2014-11-11 17:15:17 +0100

    Various changes.

--- SeminarEndlicheGruppentheorie/auto/Script-Main.el
+++ SeminarEndlicheGruppentheorie/auto/Script-Main.el
@@ -1,18 +1,9 @@
 (TeX-add-style-hook "Script-Main"
  (lambda ()
-    (TeX-add-symbols
-     "a")
     (TeX-run-style-hooks
-     "layer"
-     "mathenv"
-     "de"
-     "unicode-math"
-     "fontspec"
+     "tokenlist"
      "latex2e"
      "art10"
      "article"
-     ""
-     "../Config/math-xetex"
-     "../Config/german"
-     "../Config/index")))
+     "")))
 

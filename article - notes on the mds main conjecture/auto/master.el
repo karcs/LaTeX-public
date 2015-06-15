c938786890227a06d0d7fda3bdd186899640c67f
Jakob Schneider <jakob@karcs.wh2.tu-dresden.de>
2015-06-15 09:03:18 +0200

    Changes

--- article - notes on the mds main conjecture/auto/master.el
+++ article - notes on the mds main conjecture/auto/master.el
@@ -2,11 +2,11 @@
  "master"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("geometry" "top=0.5in" "bottom=0.5in" "left=0.5in" "right=0.5in")))
+                     '(("geometry" "left=3cm" "right=3cm" "top=0cm" "bottom=2cm")))
    (TeX-run-style-hooks
     "latex2e"
-    "../Config/german"
     "article"
     "art10"
-    "geometry")))
+    "geometry"
+    "amsmath")))
 
